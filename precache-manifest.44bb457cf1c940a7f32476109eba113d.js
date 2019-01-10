7e3f97c1a9c6c8adbd69d7ae53a10b355c20c9ec
Lex_TM <BenuelBertran@gmail.com>
2019-01-10 12:39:42 +0300

    Updates

---
+++ precache-manifest.44bb457cf1c940a7f32476109eba113d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a075fedd7e17ab7567e1",
+    "url": "/comments_gh-pages/static/js/runtime~main.a075fedd.js"
+  },
+  {
+    "revision": "dd4e93e862b584aad560",
+    "url": "/comments_gh-pages/static/js/main.dd4e93e8.chunk.js"
+  },
+  {
+    "revision": "07220eb69638ac74c097",
+    "url": "/comments_gh-pages/static/js/1.07220eb6.chunk.js"
+  },
+  {
+    "revision": "dd4e93e862b584aad560",
+    "url": "/comments_gh-pages/static/css/main.910e1016.chunk.css"
+  },
+  {
+    "revision": "22edea859f157932a0333d269e957373",
+    "url": "/comments_gh-pages/index.html"
+  }
+];
